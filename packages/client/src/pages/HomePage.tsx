7770d6c5d0d8191e48125ec46e2a6f7124a48670
Anna Rylander <annarylander@MacBook-Air-som-tillhor-Anna.local>
2022-09-27 15:35:11 +0200

    styling homepage

--- packages/client/src/pages/HomePage.tsx
+++ packages/client/src/pages/HomePage.tsx
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
 import Post from "@project1-chat-app/shared";
 import axios from "axios";
+import {
+  Input,
+  Container,
+  Button,
+  Box,
+  Heading,
+  Flex,
+  VStack,
+} from "@chakra-ui/react";
 
-axios.defaults.baseURL = "http://localhost:3002";
+axios.defaults.baseURL = "http://localhost:4000";
 
 const fetchPosts = async () => {
   const response = await axios.get<Post>("/posts");
@@ -11,23 +19,32 @@ const fetchPosts = async () => {
 };
 
 export default function HomePage() {
-    const [post, setPost] = useState<Post | undefined>();
-    const [error, setError] = useState<string | undefined>();
-  
-    useEffect(() => {
-      fetchPosts()
-        .then(setPost)
-        .catch((error) => {
-          setPost(undefined);
-          setError("Something went wrong");
-        });
-    }, []);
+  const [post, setPost] = useState<Post | undefined>();
+  const [error, setError] = useState<string | undefined>();
+
+  useEffect(() => {
+    fetchPosts()
+      .then(setPost)
+      .catch((error) => {
+        setPost(undefined);
+        setError("Something went wrong");
+      });
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {post ? post.text : error ? error : "No posts yet"}
-      </header>
-    </div>
-  )
+    <Flex bg="gray.300" justify="center" h="100vh">
+      <Container m={8}>
+        <VStack spacing={4} align="flex-start">
+          <Heading>Welcome </Heading>
+          <Input bg="white" placeholder="Write something" />
+          <Button type="submit" colorScheme="purple" width="" p={4}>
+            Send
+          </Button>
+          <Box color="gray.500" fontWeight="semibold" fontSize="s">
+            {post ? post.text : error ? error : "No posts yet"}
+          </Box>
+        </VStack>
+      </Container>
+    </Flex>
+  );
 }
