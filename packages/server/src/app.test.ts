9b67ec2508d1d6e54abfde26f7c6e2e00a2bafc3
Anna Rylander <annaelida.rylander@gmail.com>
2022-11-03 13:52:57 +0100

    unit test backend

---
+++ packages/server/src/app.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, test } from "@jest/globals";
+import { loadAllPosts } from "./models/todo-db";
+
+describe("test the server", () => {
+  test("testing jest", () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe("loading all posts", () => {
+  test("should return an array of posts", async () => {
+    const posts = await loadAllPosts();
+    expect(posts).toBeInstanceOf(Promise);
+  });
+});
