08e29d210cc28ba174281a849fbcd235fd44f01b
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-11-08 22:43:32 +0100

    Changed imports

--- packages/client/src/pages/LoginPage.tsx
+++ packages/client/src/pages/LoginPage.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { User } from "@project1-chat-app/shared";
 import {
